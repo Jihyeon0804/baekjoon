41ac185db0e711163b0457635861ac05dfc5f2d6
Jihyeon0804 <80299604+Jihyeon0804@users.noreply.github.com>
2025-06-17 14:15:49 +0900

    [level 2] Title: 올바른 괄호, Time: 6.38 ms, Memory: 54.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,16 @@
+class Solution {
+    boolean solution(String s) {
+        int sum = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                sum++;
+            } else {
+                sum--;
+            }
+            if (sum < 0) {
+                return false;
+            }
+        }
+        return sum == 0;
+    }
+}
