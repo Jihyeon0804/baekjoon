5e9d6c6ea0ba8194749e5bb4a84c7743e3420029
Jihyeon0804 <80299604+Jihyeon0804@users.noreply.github.com>
2025-06-22 23:50:54 +0900

    [Silver III] Title: N과 M (1), Time: 332 ms, Memory: 44500 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    static BufferedWriter bw;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] arr = br.readLine().split(" ");
+        int n = Integer.parseInt(arr[0]);
+        int m = Integer.parseInt(arr[1]);
+
+        List<Integer> myList = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            myList.add(i);
+        }
+
+        boolean[] visited = new boolean[n + 1];
+
+        permu(myList, new ArrayList<>(), m, visited);
+
+        bw.flush();
+        bw.close();
+
+    }
+
+    static void permu(List<Integer> myList, List<Integer> list, int target, boolean[] visited) throws IOException {
+        if (list.size() == target) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < list.size(); i++) {
+                sb.append(list.get(i));
+                if (i < list.size() - 1) sb.append(' ');
+            }
+            bw.write(sb.toString());
+            bw.newLine();
+            return;
+        }
+        for (int i = 0; i < myList.size(); i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                list.add(myList.get(i));
+                permu(myList, list, target, visited);
+                list.remove(list.size() - 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
