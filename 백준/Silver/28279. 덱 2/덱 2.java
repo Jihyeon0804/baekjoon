ddbc0ac946de6edc2ece9a6a08f35046460dcb24
Jihyeon0804 <80299604+Jihyeon0804@users.noreply.github.com>
2025-09-23 01:00:25 +0900

    [Silver IV] Title: 덱 2, Time: 1044 ms, Memory: 309780 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.java
+++ 백준/Silver/28279. 덱 2/덱 2.java
@@ -2,61 +2,47 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
         Deque<Integer> deque = new ArrayDeque<>();
-        while (n > 0) {
+        while (n-- > 0) {
             String[] arr = br.readLine().split(" ");
             int m = Integer.parseInt(arr[0]);
             switch (m) {
                 case 1:
                     deque.addFirst(Integer.parseInt(arr[1]));
-                    n--;
                     break;
                 case 2:
                     deque.addLast(Integer.parseInt(arr[1]));
-
-                    n--;
                     break;
                 case 3:
-                    if (deque.isEmpty()) {
-                        System.out.println(-1);
-                    } else {
-                        System.out.println(deque.pollFirst());
-                    }
-                    n--;
+                    sb.append(deque.isEmpty() ? -1 : deque.pollFirst()).append("\n");
                     break;
                 case 4:
-                    if (deque.isEmpty()) {
-                        System.out.println(-1);
-                    } else {
-                        System.out.println(deque.pollLast());
-                    }
-                    n--;
+                    sb.append(deque.isEmpty() ? -1 : deque.pollLast()).append("\n");
                     break;
                 case 5:
-                    System.out.println(deque.size());
-                    n--;
+                    sb.append(deque.size()).append("\n");
                     break;
                 case 6:
-                    System.out.println(deque.isEmpty() ? 1 : 0);
-                    n--;
+                    sb.append(deque.isEmpty() ? 1 : 0).append("\n");
                     break;
                 case 7:
-                    System.out.println(!deque.isEmpty() ? deque.peekFirst() : -1);
-                    n--;
+                    sb.append(!deque.isEmpty() ? deque.peekFirst() : -1).append("\n");
                     break;
                 case 8:
-                    System.out.println(!deque.isEmpty() ? deque.peekLast() : -1);
-                    n--;
+                    sb.append(!deque.isEmpty() ? deque.peekLast() : -1).append("\n");
                     break;
             }
         }
+        System.out.println(sb);
     }
 }
