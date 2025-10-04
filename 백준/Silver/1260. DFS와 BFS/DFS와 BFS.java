6a572d930e3f2381b4fd72b7de35dbc73f148390
Jihyeon0804 <80299604+Jihyeon0804@users.noreply.github.com>
2025-10-05 00:10:53 +0900

    [Silver II] Title: DFS와 BFS, Time: 244 ms, Memory: 23936 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,34 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static List<List<Integer>> adjList = new ArrayList<>();
+
+    static List<List<Integer>> adjList;
     static boolean[] visited;
-    static Queue<Integer> queue = new LinkedList<>();
     static int n, m, v;
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb1 = new StringBuilder();
+    static StringBuilder sb2 = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
 
         // DFS 와 BFS
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         n = arr[0];
         m = arr[1];
         v = arr[2];
 
-        int[][] nodes = new int[m][2];
-
-        for (int i = 0; i < nodes.length; i++) {
-            nodes[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        }
-
+        adjList = new ArrayList<>();
         for (int i = 0; i < n + 1; i++) {
             adjList.add(new ArrayList<>());
         }
 
-        for (int[] node : nodes) {
+        for (int i = 0; i < m; i++) {
+            int[] node = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             adjList.get(node[0]).add(node[1]);
             adjList.get(node[1]).add(node[0]);
         }
@@ -40,20 +37,25 @@ public class Main {
 
         visited = new boolean[n + 1];
         dfs(v);
-        bw.write("\n");
+
         visited = new boolean[n + 1];
-        bfs(v, queue);
+        bfs(v);
+
+        bw.write(sb1.toString() + "\n");
+        bw.write(sb2.toString());
         bw.flush();
         bw.close();
     }
 
-    static void bfs(int start, Queue<Integer> queue) throws IOException {
+    static void bfs(int start) throws IOException {
+        Queue<Integer> queue = new LinkedList<>();
         queue.add(start);
         visited[start] = true;
+
         while (!queue.isEmpty()) {
             int temp = queue.poll();
-            bw.write(String.valueOf(temp));
-            bw.write(" ");
+            sb2.append(temp).append(" ");
+
             for (int a : adjList.get(temp)) {
                 if (!visited[a]) {
                     queue.add(a);
@@ -64,9 +66,9 @@ public class Main {
     }
 
     static void dfs(int start) throws IOException {
-        bw.write(String.valueOf(start));
-        bw.write(" ");
+        sb1.append(start).append(" ");
         visited[start] = true;
+
         for (int i : adjList.get(start)) {
             if (!visited[i]) {
                 dfs(i);
